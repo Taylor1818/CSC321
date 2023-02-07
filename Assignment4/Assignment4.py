0fd443307449cd741bbc383d0889e539acac0d88
Taylor <tbcmorgan@gmail.com>
2023-02-06 22:36:41 -0800

    completed pass cracking

--- Assignment4/Assignment4.py
+++ Assignment4/Assignment4.py
@@ -1,89 +1,83 @@
-from math import floor
+import asyncio
 import bcrypt
 from nltk.corpus import words
-import time
 import multiprocessing
-import threading
+import time
 
 salt = None
 hash = None
+user = None
 
 def readWords():
     listOfWords = []
     for w in words.words():
-        # Only test hash word if its within char limit for password
         if (len(w) > 5) and (len(w) < 11):
             listOfWords.append(w)
     return listOfWords
 
 
-def chunckSize(wordCount):
-    return int(wordCount/multiprocessing.cpu_count())
+async def task(word_list):
+    process_list = []
+    chunk_len = int(len(word_list)/multiprocessing.cpu_count())
+    chunk1 = 0
+    chunk2 = chunk_len - 1
+
+    for core in range(multiprocessing.cpu_count()): # ran with 8 cores 
+        if (core == multiprocessing.cpu_count()):
+            p = multiprocessing.Process(
+                target=crack_pw, args=(user, salt, hash, word_list[chunk1:]))
+            process_list.append(p)
+        else:
+            p = multiprocessing.Process(target=crack_pw, args=(
+                user, salt, hash, word_list[chunk1: chunk2]))
+            process_list.append(p)
+            chunk1 += chunk_len
+            chunk2 += chunk_len
+
+        p.start()
+
+    while process_list:
+        for p in process_list:
+            if not p.is_alive():
+                process_list.remove(p)
+                for q in process_list:
+                    q.terminate()
+                break
+    return "Task completed for words: {}".format(words)
 
 
-def task(dict):
-    arr = []
-    global salt
-    global hash
-    
-    with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
-        arr = pool.map(crack_pw, [(word, salt, hash) for word in dict])
-        pool.close()
-        pool.join()
-        #call here
-    return arr
+def crack_pw(user, salt, hash, words):
+    start = time.time()
+    for word in words:
+        pwhash = bcrypt.hashpw(word.encode(), salt.encode())
+        if pwhash == (salt+hash).encode():
+            total_time = time.time() - start
+            print("Time took to find the word: '",word,"' was", total_time)
+            with open("passwords.txt", "a") as f:
+                f.write(user + " '" + word + "' \tTime: " + str(total_time) + "\n")
+            return word
 
-# #make it so that when the word is found, kills all other child processes
-def crack_pw(args):
-    word, salt, hash = args
-    pwhash = bcrypt.hashpw(word.encode(), salt.encode())
-    if pwhash == (salt+hash).encode():
-        print(word)
-        
-        # return word
 
-# def crack_pw(args, pool):
-#     word, salt, hash = args
-#     pwhash = bcrypt.hashpw(word.encode(), salt.encode())
-#     if pwhash == (salt+hash).encode():
-#         print(word)
-#         pool.terminate()
-#         return word
-
-# def task(dict, salt, hash):
-#     arr = []
-#     with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
-#         arr = pool.map(lambda x: crack_pw(x, pool), [(word, salt, hash) for word in dict])
-#         pool.terminate()
-#         pool.join()
-#     return arr
-
-def main():
-
+async def main():
     with open("shadow.txt", "r") as file:
-        for line in file: 
+        for line in file:
             i = 0
             while line[i] != '$':
                 i += 1
-
             global salt
             global hash
+            global user
             user = line[:i]
             salt = line[i:i+29]
             hash = line[i+29:-1]
 
-            
             print("\nUSER: " + user[:-1] +
                   "\nSALT: " + salt + "\nHASH: " + hash)
 
-        words = readWords()
-        words.sort(key=len)
-        task(words)
-        
-        # start = time.time()
-        # end = time.time()
-    
+            words = readWords()
+            words.sort(key=len)
+            await task(words)
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
